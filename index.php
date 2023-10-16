26aff19ccaaacc693771f26d0dbecf28c7c4d9fa
edgar <edgarnos03@gmail.com>
2023-10-16 19:47:43 +0200

    primer push

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-echo "Esto es tu indice";
+if(isset($_GET['controller'])){
+    echo 'Quieres realizar una accion sobre'.$_GET['controller'];
+    if(isset($_GET['action'])){
+        echo 'Sobre '.$_GET['controller'].' quieres mostrar la pagina'.$_GET['action'];
+    }
+}else{
+    echo 'No me has pasado controller';
+}
 ?>
